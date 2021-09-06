21cc4f5c0957c3366623e73afa92b17fc3bf5db2
dribeir0 <dribeiro1995@gmail.com>
2021-09-06 14:59:14 +0100

    feat(C): finish task c

--- src/app/task-a/task-a.component.ts
+++ src/app/task-a/task-a.component.ts
@@ -14,12 +14,18 @@ export class TaskAComponent implements OnInit, OnDestroy {
   control = new FormControl();
   subscriptions = new Subscription();
   value = false;
+  results: {expression: number, value: boolean | number}[] = [];
 
   constructor(private appService: AppService, private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.subscriptions.add(this.control.valueChanges.pipe(debounceTime(200)).subscribe((exp) => {
-      this.value = this.appService.getValueFromExpression(exp);
+      const value = this.appService.getValueFromExpression(exp); 
+      this.value = value;
+      this.results.push({expression: exp, value: value});
+      if(this.results.length > 5) {
+        this.results.shift()
+      }
       this.cd.markForCheck();
     }));
   }
